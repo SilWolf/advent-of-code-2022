8edeb09650ed268f0af37dbf803d2d4fc654a41b
SilWolf <silwolf1121@gmail.com>
2022-12-03 01:57:26 +0800

    refactor:  make a template

---
+++ src/quests/q-template/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('./input.txt', { encoding: 'utf-8' });
+const inputArgs = input.split('\n');
+
+const main = (args) => {
+	// where the logic is written...
+
+	return -1;
+};
+
+console.log(main(inputArgs));
